4cf77b665c70aa19229f33f1d2faa498b29d96f7
Bhaskar Gupta <guptabhanu1999@gmail.com>
2023-03-21 22:25:11 +0530

    something

--- process.d.ts
+++ process.d.ts
@@ -1,5 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
+    NEXTAUTH_SECRET: string;
+    NEXTAUTH_URL: string;
     AUTH0_ID: string;
     AUTH0_SECRET: string;
     AUTH0_ISSUER: string;
